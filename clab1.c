d36ae77d96017f82950050f79b27808ba983b609
Drishti-byte <drishtibansal06@gmail.com>
2024-09-23 15:36:59 +0530

    Add files via upload

---
+++ clab1.c
@@ -0,0 +1,13 @@
+/*program to print question 1*/
+#include<stdio.h>
+int main()
+{
+    printf("Drishti Bansal 1-B");
+    printf("\nHello \nWorld");
+    printf("\nHello \bworld");
+    printf("\nHello \tworld");
+    printf("\n\\Hello world");
+    printf("\n\'Hello World");
+    printf("\n\"Hello World");
+    return 0;
+}
