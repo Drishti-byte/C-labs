0638f3af7cb4d086fca799a09ba9c87cca45767c
Drishti-byte <drishtibansal06@gmail.com>
2024-09-24 18:06:51 +0530

    Add files via upload

---
+++ clab2.c
@@ -0,0 +1,11 @@
+/*program to print question 2*/
+#include<stdio.h>
+int main()
+{
+    printf("Drishti Bansal 1-B");
+    int a=10;
+    float b=2.5;
+    printf("\na is %d",a);
+    printf("\nb is %f",b);
+    return 0;
+}
