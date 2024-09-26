70a44a56c0a34ca5afad8da54832c0fd08452d3a
Drishti-byte <drishtibansal06@gmail.com>
2024-09-26 16:30:00 +0530

    Add files via upload

---
+++ clab4.c
@@ -0,0 +1,16 @@
+/*program to print question 4*/
+#include<stdio.h>
+int main()
+{
+    printf("Drishti Bansal 1-B");
+    int a,b,c;
+    printf("\nEnter a:");
+    scanf("%d",&a);
+    printf("\nEnter b:");
+    scanf("%d",&b);
+    printf("\na is %d",a);
+    printf("\nb is %d",b);
+    c=a+b;
+    printf("\nSum is %d",c);
+    return 0;
+}
