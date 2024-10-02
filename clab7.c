506f1be9ae960d3f2ea3879b4bcb2329fdedc2f4
Drishti-byte <drishtibansal06@gmail.com>
2024-10-02 13:18:26 +0530

    Add files via upload

---
+++ clab7.c
@@ -0,0 +1,15 @@
+/*progarm to print question 7*/
+#include<stdio.h>
+int main()
+{
+    printf("Drishti Bansal 1-B");
+    int p,r,t,si;
+    r=10;
+    printf("\nEnter p:");
+    scanf("%d",&p);
+    printf("\nEnter t:");
+    scanf("%d",&t);
+    si=p*r*t/100;
+    printf("\nSi is %d",si);
+    return 0;
+}
